e4dfaaa2b36d2c0ab21315768e8258ad48163b58
Ethan Hadley <ekhadley@gmail.com>
2025-05-14 19:52:29 -0500

    r

--- supervised_rollout_think.py
+++ supervised_rollout_think.py
@@ -128,72 +128,69 @@ def train(model: GPT2Thinking, cfg: TrainingConfig, dataset: datasets.Dataset, s
             # The prediction on the end_thought sequence position is the real next token prediction.
             model.printSeq(full_seq) 
             
-            # ctx holds all our tokens. We generate it withou gradients during the inference step, then clone it.
-            # For a sequence of length s, we perform s rollouts. So each row of ctx is an input subsequence followed by a rollout, ending with the end_thought token.
-            # ctx has s rows.
-            ctx_g = ctx.clone()
-            logits = model(ctx_g) # These are the model's logits (with gradients) on the ctx sequence.
+        # ctx holds all our tokens. We generate it withou gradients during the inference step, then clone it.
+        # For a sequence of length s, we perform s rollouts. So each row of ctx is an input subsequence followed by a rollout, ending with the end_thought token.
+        # ctx has s rows.
+        ctx_g = ctx.clone()
+        logits = model(ctx_g) # These are the model's logits (with gradients) on the ctx sequence.
 
-            z = 0
-            last_tt = ctx[z, end_indices[z] - 1].detach().item()
-            print(purple, f"{last_tt=}", endc)
-            pred_nt = logits[z, end_indices[z]].argmax().detach().item()
-            real_nt = seq[z + 1].detach().item()
-            print(yellow, f"{logits.shape=}, {ctx.shape=}, {end_indices.shape=}, {seq.shape=}", endc)
-            print(pink, f"{end_indices[z]}", endc)
-            print(magenta, f"{ctx[z, end_indices[z]]=}", endc)
-            print(f"{purple}start: {z}, end: {end_indices[z]}, predicted real tok: {pred_nt}('{model.tokenizer.decode(pred_nt)}') with logit {logits[z, end_indices[z] - 1, pred_nt]}, real next tok: {real_nt}('{model.tokenizer.decode(real_nt)}'), logit on real next tok: {logits[z, end_indices[z]-1, real_nt]}{endc}")
-            
-            # These are the models next-token logits and logprobs for each token in each sequence.
-            ctx_logits = logits[seq_indices_m1[:, None], seq_indices_m1[None, :], ctx_g[:, 1:]] 
-            ctx_logprobs = t.log_softmax(ctx_logits, dim=-1)
+        #z = 0
+        #last_tt = ctx[z, end_indices[z] - 1].detach().item()
+        #print(purple, f"{last_tt=}", endc)
+        #pred_nt = logits[z, end_indices[z]].argmax().detach().item()
+        #real_nt = seq[z + 1].detach().item()
+        #print(yellow, f"{logits.shape=}, {ctx.shape=}, {end_indices.shape=}, {seq.shape=}", endc)
+        #print(pink, f"{end_indices[z]}", endc)
+        #print(magenta, f"{ctx[z, end_indices[z]]=}", endc)
+        #print(f"{purple}start: {z}, end: {end_indices[z]}, predicted real tok: {pred_nt}('{model.tokenizer.decode(pred_nt)}') with logit {logits[z, end_indices[z] - 1, pred_nt]}, real next tok: {real_nt}('{model.tokenizer.decode(real_nt)}'), logit on real next tok: {logits[z, end_indices[z]-1, real_nt]}{endc}")
+        
+        # These are the models next-token logits and logprobs for each token in each sequence.
+        ctx_logits = logits[seq_indices_m1[:, None], seq_indices_m1[None, :], ctx_g[:, 1:]] 
+        ctx_logprobs = t.log_softmax(ctx_logits, dim=-1)
 
-            # The logit value at the end_thought token position corresponding to the true next token. We want to maximize these: the logit for the actual next token. These are our rewards.
-            pred_logits = logits[seq_indices_m1, end_indices, ctx[-1, 1:]]
-            logit_mean, logit_std = pred_logits.mean().detach(), pred_logits.std().detach()
-            # normalize rewards across all the rollouts. The think token logprobs of the top half
-            # (in terms of logit on correct token on the end_thought position) are reinforced, the bottom half are pushed down.
-            rewards = (pred_logits - logit_mean) / (logit_std + 1e-8) 
+        # The logit value at the end_thought token position corresponding to the true next token. We want to maximize these: the logit for the actual next token. These are our rewards.
+        pred_logits = logits[seq_indices_m1, end_indices, ctx[-1, 1:]]
+        logit_mean, logit_std = pred_logits.mean().detach(), pred_logits.std().detach()
+        # normalize rewards across all the rollouts. The think token logprobs of the top half
+        # (in terms of logit on correct token on the end_thought position) are reinforced, the bottom half are pushed down.
+        rewards = (pred_logits - logit_mean) / (logit_std + 1e-8) 
 
-            print(cyan, rewards.shape, endc)
-            line(rewards)
+        #print(cyan, rewards.shape, endc)
+        #line(rewards)
 
-            # action logprobs times normalized rewards. we want to maximize this.
-            # strangely though, here the rewards are differentiable as well.
-            weighted_logprobs = ctx_logprobs * rewards.unsqueeze(1) 
-
-
-            loss = weighted_logprobs.mean()
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-
-            print(cyan, logits[z, end_indices[z], real_nt], endc)
-            print(lime, pred_logits, endc)
-            print(red, seq_indices_m1[:10], blue, end_indices[:10], green, ctx[-1, :10], endc)
-
-            line(pred_logits)
+        # action logprobs times normalized rewards. we want to maximize this.
+        # strangely though, here the rewards are differentiable as well.
+        weighted_logprobs = ctx_logprobs * rewards.unsqueeze(1) 
 
 
-            imshow(ctx_logits, title=f"ctx_logits ({ctx_logits.shape})")
-            imshow(ctx_logprobs, title=f"ctx_logprobs ({ctx_logprobs.shape})")
-            imshow(weighted_logprobs, title=f"weighted_logprobs ({weighted_logprobs.shape})")
-            imshow(ctx, title=f"tokens ({ctx.shape})")
-            ctx_map = t.zeros_like(ctx)
-            ctx_map[ctx > model.eot] = 1
-            ctx_map[ctx < model.eot] = -1
-            ctx_map[ctx == model.eot] = 0
-            ctx_map[ctx == model.end_thought] = -2
-            imshow(ctx_map, title=f"ctx_map ({ctx_map.shape})")
+        loss = weighted_logprobs.mean()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-            #discounts = t.zeros_like(ctx_logits)
-            #for i in range(seq_len - 1):
-            #     discounts[i, i:end_indices[i]] = t.pow(cfg.gamma, t.arange(end_indices[i] - i)).flip(dims=(0,))
-            #imshow(discounts, title=f"discounts ({discounts.shape})")
+        #print(cyan, logits[z, end_indices[z], real_nt], endc)
+        #print(lime, pred_logits, endc)
+        #print(red, seq_indices_m1[:10], blue, end_indices[:10], green, ctx[-1, :10], endc)
+        #line(pred_logits)
+        #imshow(ctx_logits, title=f"ctx_logits ({ctx_logits.shape})")
+        #imshow(ctx_logprobs, title=f"ctx_logprobs ({ctx_logprobs.shape})")
+        #imshow(weighted_logprobs, title=f"weighted_logprobs ({weighted_logprobs.shape})")
+        #imshow(ctx, title=f"tokens ({ctx.shape})")
+        #ctx_map = t.zeros_like(ctx)
+        #ctx_map[ctx > model.eot] = 1
+        #ctx_map[ctx < model.eot] = -1
+        #ctx_map[ctx == model.eot] = 0
+        #ctx_map[ctx == model.end_thought] = -2
+        #imshow(ctx_map, title=f"ctx_map ({ctx_map.shape})")
 
-            #weighted_ctx_logits = ctx_logits * discounts
-            #imshow(weighted_ctx_logits, title=f"weighted_ctx_logits ({weighted_ctx_logits.shape})")
-            exit()
+        #discounts = t.zeros_like(ctx_logits)
+        #for i in range(seq_len - 1):
+        #     discounts[i, i:end_indices[i]] = t.pow(cfg.gamma, t.arange(end_indices[i] - i)).flip(dims=(0,))
+        #imshow(discounts, title=f"discounts ({discounts.shape})")
+
+        #weighted_ctx_logits = ctx_logits * discounts
+        #imshow(weighted_ctx_logits, title=f"weighted_ctx_logits ({weighted_ctx_logits.shape})")
+        exit()
 
 if __name__ == "__main__":
     model_cfg = ThinkingModelConfig(d_model=512, seq_len=128, d_mlp=2048, d_head=64, n_heads=8, n_layers=8, d_normal_vocab=50257, d_thought_vocab=2048)
