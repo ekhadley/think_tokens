e41b73f3283ef6063d41c4c7352245fd66abe4a5
ehadley <ehadley@cc-gpu-02.hpc.siue.edu>
2025-05-17 13:33:28 -0500

    r

--- supervised_rollout_think.py
+++ supervised_rollout_think.py
@@ -183,7 +183,7 @@ def train(model: GPT2Thinking, cfg: TrainingConfig, dataset: datasets.Dataset, s
         optimizer.step()
         optimizer.zero_grad()
         #imshow(weighted_logprobs, title=f"weighted_logprobs ({weighted_logprobs.shape})")
-        exit()
+        #exit()
 
 if __name__ == "__main__":
     model_cfg = ThinkingModelConfig(d_model=512, seq_len=128, d_mlp=2048, d_head=64, n_heads=4, n_layers=4, d_normal_vocab=50257, d_thought_vocab=2048)
