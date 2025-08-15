7393cbce035c180561519c4912a3d03e2a270424
Ethan Hadley <ekhadley@gmail.com>
2025-08-15 17:34:00 -0500

    no more chess

--- normal.py
+++ normal.py
@@ -6,60 +6,53 @@ import datasets
 from utils import *
 from models import GPT2, TrainingConfig, ModelConfig
 
-def train(model, cfg: TrainingConfig, dataset: datasets.Dataset):
+def train(model: GPT2, cfg: TrainingConfig, dataset: datasets.Dataset):
     optimizer = t.optim.AdamW(model.parameters(), lr=cfg.lr, betas=(cfg.adam_beta1, cfg.adam_beta2), weight_decay=cfg.weight_decay)
 
     model.train()
 
-    wandb.init(project="thoughtful", name="normal", config=cfg)
-    wandb.watch(model, log="all")
-    wandb.config.update(model.cfg.to_dict())
-    wandb.config.update(cfg.to_dict())
-
-    sample_completion = model.yap("George Washington was")
-    print(yellow, sample_completion, endc)
-    table_data = [[sample_completion]]
-    table = wandb.Table(data=table_data, columns=['completion'])
-    wandb.log({"sample_completion": table})
+    run_cfg = {"model": model.cfg.to_dict(), "training": cfg.to_dict()}
+    wandb.init(project="recycler", name="normal", config=run_cfg)
 
     dl = t.utils.data.DataLoader(dataset, batch_size=cfg.batch_size)
     for i, batch in enumerate((tr:=tqdm.tqdm(dl, ncols=100))):
-        tokens = batch['input_ids']
+        tokens = batch['input_ids'].to(model.embed.weight.device)
+        batch_size, seq_len = tokens.shape
+
         logits = model(tokens)
         logprobs = t.log_softmax(logits[:, :-1], dim=-1)
-        loss = -eindex.eindex(logprobs, tokens[:, 1:], "batch seq [batch seq]").mean()
-        optimizer.zero_grad()
+        loss = -logprobs[t.arange(batch_size).unsqueeze(-1), t.arange(seq_len - 1).unsqueeze(0), tokens[:, 1:]].mean()
         loss.backward()
+        grad_norm = t.nn.utils.clip_grad_norm_(model.parameters(), max_norm=2.0, error_if_nonfinite=True)
+
         optimizer.step()
+        optimizer.zero_grad()
 
-        wandb.log({"loss": loss.item()})
-        tr.set_description(f"{magenta}loss: {loss.item():.3f}")
-
-        if i%1_000 == 0:
-            sample_completion = model.yap("George Washington was")
-            print(yellow, sample_completion, endc)
-            table_data.append([sample_completion])
-            table = wandb.Table(data=table_data, columns=['completion'])
-            wandb.log({"sample_completion": table})
-
-            t.save(model.state_dict(), f"saves/normal{i}.pth")
+        wandb.log({"loss": loss.item(), "grad_norm": grad_norm})
+        tr.set_description(f"{magenta}loss: {loss.item():.3f}, grad_norm: {grad_norm:.3f}")
 
 if __name__ == "__main__":
     t.set_default_device(t.device("cuda"))
     t.manual_seed(42)
     random.seed(42)
 
-    model_cfg = ModelConfig(d_model=512, seq_len=256, d_mlp=2048, n_heads=8, n_layers=8, d_vocab=64)
+    d_model = 512
+    model_cfg = ModelConfig(
+        d_model=d_model,
+        seq_len=256,
+        d_mlp=d_model * 4,
+        n_heads=8,
+        n_layers=12,
+        d_vocab=50_257
+    )
     model = GPT2(model_cfg)
     training_cfg = TrainingConfig(
         batch_size=64,
         lr=1e-3,
         weight_decay=1e-6,
-        adam_beta1=0.9,
-        adam_beta2=0.95
     )
 
-    dataset = tokenizeAndSaveDataset(model.tokenizer, model_cfg, "HuggingFaceFW/fineweb-edu", "sample-10BT", f"fineweb-edu-tokenized-512", 0.07, pad=False)
-    #dataset = loadTokenizedDataset("fineweb-edu-tokenized-256")
+    #dataset = tokenizeAndSaveDataset(model.tokenizer, model_cfg, "HuggingFaceFW/fineweb-edu", "sample-10BT", f"fineweb-edu-tokenized-512", 0.07, pad=False)
+    dataset = loadTokenizedDataset("fineweb-edu-tokenized-256")
     
     train(model, training_cfg, dataset)
---
+++ recycler.py
@@ -0,0 +1,96 @@
+import time
+import eindex
+import wandb
+import tqdm
+import datasets
+from typing import Callable
+
+from utils import *
+from models import Recycler, RecycleModelConfig, TrainingConfig
+
+
+def train(model: Recycler, cfg: TrainingConfig, trainset: datasets.Dataset):
+    optimizer = t.optim.AdamW(model.parameters(), lr=cfg.lr, betas=(cfg.adam_beta1, cfg.adam_beta2), weight_decay=cfg.weight_decay)
+
+    model.train()
+
+    run_cfg = {"model": model.cfg.to_dict(), "training": cfg.to_dict()}
+    wandb.init(project="recycler", name="recycler", config=run_cfg)
+
+    batch_size = cfg.batch_size
+    seq_len = trainset['input_ids'].shape[1]
+    d_model = model.cfg.d_model
+    d_vocab = model.cfg.d_vocab
+
+    seq_indices = t.arange(seq_len - 1, requires_grad=False)
+    grad_norm = 0
+
+    dl = t.utils.data.DataLoader(trainset, batch_size=cfg.batch_size)
+    for i, batch in enumerate((tr:=tqdm.tqdm(dl, ncols=100))):
+        tokens = batch['input_ids'].to(model.embed.weight.device)
+        batch_size, seq_len = tokens.shape
+
+        context_parts: list[t.Tensor] = []
+        logit_parts: list[t.Tensor] = []
+        #for s in range(seq_len): # this one is for non-interleaved embedding approaches
+            #next_toks = tokens[:, s].reshape(batch_size)
+            #cur_toks = tokens[:, :s+1]
+            #context = t.cat(context_parts, dim=1) if s != 0 else None
+            ##new_ctx, new_logits = model.forward_replace_embeddings(next_toks, context)
+            #new_ctx, new_logits = model.forward_attn_gate(cur_toks, context)
+            #context_parts.append(new_ctx.unsqueeze(1))
+            #logit_parts.append(new_logits.unsqueeze(1))
+
+        for s in range(seq_len): # for interleaved embedding approaches
+            next_toks = tokens[:, s].reshape(batch_size)
+            cur_toks = tokens[:, :s+1]
+            context = t.cat(context_parts, dim=1) if s > 0 else None
+            new_ctx, new_logits = model.forward_interleaved_embeddings(next_toks, context)
+            #new_ctx, new_logits = model.forward_attn_gate_interleaved(cur_toks, context)
+            #new_ctx, new_logits = model.forward_recycler_block_interleaved(next_toks, context)
+            logit_parts.append(new_logits.unsqueeze(1))
+            
+            tok_embeds = model.embed(next_toks).reshape(batch_size, d_model)
+            context_parts.append(tok_embeds.unsqueeze(1))
+            context_parts.append(new_ctx.unsqueeze(1))
+        
+        logits = t.cat(logit_parts, dim=1)
+        logprobs = t.log_softmax(logits, dim=-1)
+        loss = -logprobs[t.arange(batch_size).unsqueeze(-1), t.arange(seq_len - 1).unsqueeze(0), tokens[:, 1:]].mean()
+        loss.backward()
+        grad_norm = t.nn.utils.clip_grad_norm_(model.parameters(), max_norm=2.0, error_if_nonfinite=True)
+
+        optimizer.step()    
+        optimizer.zero_grad()
+
+        wandb.log({"loss": loss.item(), "grad_norm": grad_norm})
+        tr.set_description(f"{magenta}loss: {loss.item():.3f}, grad_norm: {grad_norm:.3f}")
+
+
+if __name__ == "__main__":
+    t.set_default_device(t.device("cuda"))
+    t.manual_seed(42)
+    random.seed(42)
+
+    d_model = 512
+    model_cfg = RecycleModelConfig(
+        d_model=d_model,
+        seq_len=256,
+        d_mlp=d_model * 4,
+        n_heads=8,
+        n_layers=12,
+        recycle_layer=9,
+        d_vocab=50_257
+    )
+    model = Recycler(model_cfg)
+
+    training_cfg = TrainingConfig(
+        batch_size=64,
+        lr=3e-3,
+        weight_decay=1e-4,
+    )
+
+    #dataset = tokenizeAndSaveDataset(model.tokenizer, model_cfg, "HuggingFaceFW/fineweb-edu", "sample-10BT", f"fineweb-edu-tokenized-512", 0.07, pad=False)
+    dataset = loadTokenizedDataset("fineweb-edu-tokenized-256")
+    
+    train(model, training_cfg, dataset)
